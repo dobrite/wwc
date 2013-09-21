0c3ae0a98e33a60af440f053301c2cfecaeedca1
Dave O <davidobrite@gmail.com>
2013-09-21 07:24:42 -0700

    formatting

--- app/scripts/controllers/loginController.js
+++ app/scripts/controllers/loginController.js
@@ -1,19 +1,20 @@
-(function() {
-	'use strict';
+(function () {
+    'use strict';
 
-	var root = this;
+    var root = this;
 
-	root.define([
-		'backbone'
-	],
-	function( Backbone ) {
+    root.define([
+        'backbone'
+    ],
+    function(Backbone) {
 
-		return Backbone.Marionette.Controller.extend({
-		
-			initialize: function( options ) {
-				console.log("initialize a Logincontroller Controller");
-			}
-		});
+        return Backbone.Marionette.Controller.extend({
 
-	});
-}).call( this );
+            initialize: function(options) {
+                console.log("initialize a LoginController Controller");
+            }
+
+        });
+
+    });
+}).call(this);
