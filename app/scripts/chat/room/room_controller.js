e355baeec5475b82ecbfa157dbff3d764606b945
Dave O <davidobrite@gmail.com>
2013-10-13 06:30:48 -0700

    formatting

--- app/scripts/chat/room/room_controller.js
+++ app/scripts/chat/room/room_controller.js
@@ -1,31 +1,31 @@
 define([
-        "backbone",
-        "scripts/application",
-        "scripts/communicator",
-        "scripts/main_router",
-    ],
-    function(Backbone, app, communicator, mainRouter){
+    "backbone",
+    "scripts/application",
+    "scripts/communicator",
+    "scripts/main_router",
+],
+function(Backbone, app, communicator, mainRouter){
 
-        var RoomController = Backbone.Marionette.Controller.extend({
+    var RoomController = Backbone.Marionette.Controller.extend({
 
-            initialize: function (options) {
-                this.room = options.room;
-                //subscribe to room
-            },
+        initialize: function (options) {
+            this.room = options.room;
+            //subscribe to room
+        },
 
-            showRoom: function () {
-                var messageView = new MessageView();
-                var nickView = new NickView();
+        showRoom: function () {
+            var messageView = new MessageView();
+            var nickView = new NickView();
 
-                var roomLayout = new RoomLayout();
+            var roomLayout = new RoomLayout();
 
-                //show layout on region
-            },
+            //show layout on region
+        },
 
-            onClose: function () {
-            },
+        onClose: function () {
+        },
 
-        });
+    });
 
-        return RoomController;
+    return RoomController;
 });
