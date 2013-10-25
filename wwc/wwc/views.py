ea5269cede3e89e5652dfc267d0e817645974123
Dave O <davidobrite@gmail.com>
2013-10-25 06:51:24 -0700

    cleanup

--- wwc/wwc/views.py
+++ wwc/wwc/views.py
@@ -39,37 +39,6 @@ def index_view(request):
 
 
 @view_config(
-    route_name='test',
-    renderer='wwc:templates/result.mak',
-)
-def test_view(request):
-    schema = RedditLoginSchema()
-    form = Form(schema, buttons=('submit',))
-    username = "username"
-    reqts = form.get_widget_resources()
-    return {
-        'form': form.render({'username': username}),
-        'reqts': reqts
-    }
-
-
-#@view_config(
-#    context='velruse.providers.reddit.RedditAuthenticationComplete',
-#    renderer='wwc:templates/result.mak',
-#)
-#def reddit_login_complete_view(request):
-#    schema = RedditLoginSchema()
-#    form = Form(schema, buttons=('submit',))
-#    reqts = form.get_widget_resources()
-#    context = request.context
-#    username = context.profile['preferredUsername']
-#    return {
-#        'form': form.render({'username': username}),
-#        'reqts': reqts,
-#    }
-
-
-@view_config(
     context='velruse.providers.reddit.RedditAuthenticationComplete',
     renderer='wwc:templates/result.mak',
 )
@@ -84,40 +53,6 @@ def reddit_login_complete_view(request):
     redirect.set_cookie('wwc.project_id', project_id)
     return redirect
 
-    """
-    compute token
-    redirect or serve backbone w/ inserted into template:
-    we could check websocket support for url
-    no debug - or debug if development.ini
-
-    var config = {
-        namespace: 'test',
-        url: 'http://localhost:8000/connection',
-        token: '15f928437b0fa1fdd58921f19c854f29',
-        project: '52522b73a4dd5f27c53999d6',
-        user: '2694',
-        debug: true,
-    };
-
-    return config;
-    """
-
-#@view_config(
-#    route_name='reddit_register',
-#    renderer='wwc:templates/result.mak',
-#)
-#def reddit_register(self):
-#    pass
-#if 'submit' in request.params:
-#    controls = request.POST.items()
-#    try:
-#        appstruct = form.validate(controls)
-#    except deform.ValidationFailure as e:
-#        return {
-#            'form': e.render(),
-#            'reqts': reqts,
-#        }
-
 
 @view_config(
     context='velruse.AuthenticationDenied',
@@ -125,9 +60,3 @@ def reddit_login_complete_view(request):
 )
 def login_complete_view(request):
     return {'result': 'denied', }
-
-
-@view_config(route_name='all', renderer='json')
-def all(request):
-    user = User.get_all(request).all()
-    return user[0].__json__(request, convert_date=True)
