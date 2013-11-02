0dbec399bc8fc16a4555ca0ca78dee4ce7bd5656
Dave O <davidobrite@gmail.com>
2013-11-02 04:59:11 -0700

    removed config from application

--- app/scripts/application.js
+++ app/scripts/application.js
@@ -19,17 +19,16 @@ function (Backbone, communicator, regionManager, WebsocketProxy) {
 
     app.on("initialize:after", function () {
         require([
-            "scripts/chat_config",
             "scripts/ws/websocket_api",
             "scripts/chat/chat_app",
             "scripts/login/login_app",
             "scripts/entities/room_entities",
         ],
-        function (config) {
+        function () {
             communicator.command.execute("router:history:start");
 
             if(communicator.reqres.request("router:route") === ""){
-                communicator.vent.trigger("login:show", config.user);
+                communicator.vent.trigger("login:show");
             }
 
         });
