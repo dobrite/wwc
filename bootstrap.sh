1358395330daabc5f7c8117b401ff804476b347e
Dave O <davidobrite@gmail.com>
2013-11-14 21:09:49 -0800

    update install script for redis

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,16 +4,15 @@ apt-get update
 
 # build tool chain
 apt-get install -y make build-essential
+apt-get install -y git
 
 # for python 3
 apt-get install -y openssl libssl-dev libbz2-dev zlib1g-dev libexpat1-dev libdb4.8-dev libsqlite3-dev libncursesw5-dev libncurses5-dev libreadline-dev libxml2-dev python-software-properties
 #
 # compile python 3.3
-wget http://python.org/ftp/python/3.3.2/Python-3.3.2.tar.bz2
-mv Python-3.3.2.tar.bz2 /tmp
 cd /tmp
-tar jxf Python-3.3.2.tar.bz2
-cd Python-3.3.2
+wget http://python.org/ftp/python/3.3.2/Python-3.3.2.tar.bz2
+tar jxf Python-3.3.2.tar.bz2 --strip-components 1
 ./configure --prefix=/opt/python3.3
 make && make install
 ln -s /opt/python3.3/bin/pyvenv /usr/local/bin
@@ -30,27 +29,25 @@ apt-get install -y postgresql-9.3
 cd /vagrant
 
 # redis 2.8.0-rc5
-# could use chris-lea/redis-server
+# could use chris-lea/redis-server once it's released
 cd /opt
 mkdir redis
 chown vagrant:vagrant redis
 cd redis
 wget http://download.redis.io/releases/redis-2.8.0-rc5.tar.gz
-tar xfvz redis-2.8.0-rc5.tar.gz
-cd redis-2.8.0-rc5
-make
-src/redis-server --daemonize yes
+tar xfvz redis-2.8.0-rc5.tar.gz --strip-components 1
+make && make install
+cd utils
+echo "\n\n\n\n\n" | sudo ./install_server.sh
 
-cd /vagrant
+#src/redis-server --daemonize yes
+#port 6379
+#config /etc/redis/6379.conf
+#log /var/log/redis_6379.log
+#data directory /var/lib/redis/6379
+#redis executable path /opt/redis/src/redis-server
 
 # nodejs, includes npm and nodejs-dev
 add-apt-repository -y ppa:chris-lea/node.js
 sudo apt-get update
 sudo apt-get -y install nodejs
-
-cd /vagrant
-
-# bower
-#npm install -g bower
-
-apt-get install -y git
