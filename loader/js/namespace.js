cf72106c003a0ea122cb88e4e112a571d6a89e0b
Dave O <davidobrite@gmail.com>
2013-12-03 19:13:21 -0800

    moved loader

--- loader/js/namespace.js
+++
@@ -1,5 +0,0 @@
-// Wrapper method to get the namespace universal, just so noone is tempted to
-// start playing with the `window` object itself.
-define('namespace', function() {
-  return namespace;
-});
--- loader/js/require.js
+++
@@ -1,5 +0,0 @@
-if(!namespace.define) {
-  namespace.define = define;
-  namespace.require = require;
-  namespace.requirejs = requirejs;
-}
